d726ac5aa4942e6cc39f28b61b0d31d3f662456c
hexxa <hexxa@outlook.com>
2020-12-06 18:51:06 +0800

    chore(pkgs): remove useless pkgs

--- src/logging/log_interface.go
+++
@@ -1,9 +0,0 @@
-package logging
-
-type ILogger interface {
-	Debug()
-	Log(values ...interface{})
-	Logf(pattern string, values ...interface{})
-	Error(values ...interface{})
-	Errorf(pattern string, values ...interface{})
-}
