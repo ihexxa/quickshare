047e3a860179e8ae346faa4a20ca71ce06b519ad
Hexxa <hexxa@outlook.com>
2021-05-21 15:19:28 +0800

    fix(client/web): remove css loader (#57)

--- src/client/web/src/app.tsx
+++ src/client/web/src/app.tsx
@@ -3,12 +3,12 @@ import * as ReactDOM from "react-dom";
 
 import { StateMgr } from "./components/state_mgr";
 
-import "./theme/reset.css";
-import "./theme/white.css";
+// import "./theme/reset.css";
+// import "./theme/white.css";
 // TODO: it fails in jest preprocessor now
-import "./theme/style.css";
-import "./theme/desktop.css";
-import "./theme/color.css";
+// import "./theme/style.css";
+// import "./theme/desktop.css";
+// import "./theme/color.css";
 
 
 ReactDOM.render(<StateMgr />, document.getElementById("mount"));
