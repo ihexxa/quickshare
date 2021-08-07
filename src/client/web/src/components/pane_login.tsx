29da72731ffe66c047519a1a4b66b0aa45957135
hexxa <hexxa@outlook.com>
2021-08-07 10:36:41 +0800

    fix(ui): fix login pane layout

--- src/client/web/src/components/pane_login.tsx
+++ src/client/web/src/components/pane_login.tsx
@@ -163,39 +163,52 @@ export class AuthPane extends React.Component<Props, State, {}> {
           style={{ display: this.props.authed ? "none" : "block" }}
         >
           <div className="padding-l">
-            <input
-              name="user"
-              type="text"
-              onChange={this.changeUser}
-              value={this.state.user}
-              className="black0-font margin-t-m margin-b-m"
-              placeholder="user name"
-            />
-            <input
-              name="pwd"
-              type="password"
-              onChange={this.changePwd}
-              value={this.state.pwd}
-              className="black0-font margin-t-m margin-b-m"
-              placeholder="password"
-            />
-            <input
-              name="captcha"
-              type="text"
-              onChange={this.changeCaptcha}
-              value={this.state.captchaInput}
-              className="black0-font margin-t-m margin-b-m"
-              placeholder="captcha"
-            />
-            <img
-            src={`/v1/captchas/imgs?capid=${this.props.captchaID}`}
-            />
-            <button
-              onClick={this.login}
-              className="green0-bg white-font margin-t-m margin-b-m"
-            >
-              Log in
-            </button>
+            <div className="flex-list-container">
+              <div className="flex-list-item-l">
+                <input
+                  name="user"
+                  type="text"
+                  onChange={this.changeUser}
+                  value={this.state.user}
+                  className="black0-font margin-t-m margin-b-m margin-r-m"
+                  placeholder="user name"
+                />
+                <input
+                  name="pwd"
+                  type="password"
+                  onChange={this.changePwd}
+                  value={this.state.pwd}
+                  className="black0-font margin-t-m margin-b-m"
+                  placeholder="password"
+                />
+              </div>
+              <div className="flex-list-item-r">
+                <button
+                  onClick={this.login}
+                  className="green0-bg white-font margin-t-m margin-b-m"
+                >
+                  Log in
+                </button>
+              </div>
+            </div>
+
+            <div className="flex-list-container">
+              <div className="flex-list-item-l">
+                <input
+                  name="captcha"
+                  type="text"
+                  onChange={this.changeCaptcha}
+                  value={this.state.captchaInput}
+                  className="black0-font margin-t-m margin-b-m margin-r-m"
+                  placeholder="captcha"
+                />
+                <img
+                  src={`/v1/captchas/imgs?capid=${this.props.captchaID}`}
+                  className="captcha"
+                />
+              </div>
+              <div className="flex-list-item-l"></div>
+            </div>
           </div>
         </div>
 
