a790555a60e152ffb58a38478b5beb77f0962db1
hexxa <hexxa@outlook.com>
2021-08-25 18:47:53 +0800

    feat(ui): add i18n support

---
+++ src/client/web/src/i18n/zh_CN.ts
@@ -0,0 +1,8 @@
+import { Map } from "immutable";
+
+const adContent = `
+发现bug等, 可至下方github库提交issue, 不过周末可能不上班😎.`;
+
+export const msgs: Map<string, string> = Map({
+  "loader.top.about": "关于",
+});
