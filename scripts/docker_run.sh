2dc156cd954a4c62af263994dea1e01b9c003b08
hexxa <hexxa@outlook.com>
2021-09-14 16:24:14 +0800

    fix(cicd): refactor scripts for cicd

--- scripts/docker_run.sh
+++
@@ -1,7 +0,0 @@
-docker run \
-    --name quickshare \
-    -d -p :8686:8686 \
-    -v `pwd`/quickshare/root:/quickshare/root \
-    -e DEFAULTADMIN=qs \
-    -e DEFAULTADMINPWD=1234 \
-    hexxa/quickshare
