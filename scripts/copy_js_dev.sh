82a52b2312a6b39ad5b0f96f8a0bbc92d3d1dcfa
hexxa <hexxa@outlook.com>
2021-09-14 17:24:00 +0800

    fix(cd): move yarn to package.json script

--- scripts/copy_js_dev.sh
+++ scripts/copy_js_dev.sh
@@ -2,7 +2,6 @@
 set -o nounset errexit
 
 export ROOT=`pwd`
-yarn
 cp $ROOT/node_modules/immutable/dist/immutable.min.js $ROOT/public/static/js/
 cp $ROOT/node_modules/react-dom/umd/react-dom.development.js $ROOT/public/static/js/
 cp $ROOT/node_modules/react/umd/react.development.js $ROOT/public/static/js/
