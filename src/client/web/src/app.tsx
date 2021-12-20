c94de25ac22a936bbbc4947de3aca8c11f81c79b
hexxa <hexxa@outlook.com>
2021-12-20 16:11:13 +0800

    feat(fe/app): add global error listener

--- src/client/web/src/app.tsx
+++ src/client/web/src/app.tsx
@@ -2,13 +2,21 @@ import * as React from "react";
 import * as ReactDOM from "react-dom";
 
 import { StateMgr } from "./components/state_mgr";
+import { ErrorLogger } from "./common/log_error";
+import { errCorsScript } from "./common/errors";
 
-// import "./theme/reset.css";
-// import "./theme/white.css";
-// TODO: it fails in jest preprocessor now
-// import "./theme/style.css";
-// import "./theme/desktop.css";
-// import "./theme/color.css";
-
+window.onerror = (
+  msg: string,
+  source: string,
+  lineno: number,
+  colno: number,
+  error: Error
+) => {
+  const lowerMsg = msg.toLowerCase();
+  if (lowerMsg.indexOf("script error") > -1) {
+    ErrorLogger().error(errCorsScript, "Check Browser Console for Detail");
+  }
+  ErrorLogger().error(`${source}:${lineno}:${colno}: ${error.toString()}`, "");
+};
 
 ReactDOM.render(<StateMgr />, document.getElementById("mount"));
