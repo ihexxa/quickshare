e830611af1dad2fef5571f73ef7175935112810f
hexxa <hexxa@outlook.com>
2022-02-07 21:44:20 +0800

    fix(cd): 1.add default user in docker image for avoiding permission issues 2.add demo Dockerfile

--- scripts/build_exec.sh
+++ scripts/build_exec.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 set -o nounset errexit
 
-export QSROOT=`pwd`
+export QSROOT
+QSROOT=$(pwd)
 rm -r dist && mkdir dist
 docker builder prune
 
-# set this for builders behind GFW...
+# set this for users behind GFW...
 go env -w GOPROXY=https://goproxy.cn,direct
 go get -d -v ./...
 go get github.com/mitchellh/gox
