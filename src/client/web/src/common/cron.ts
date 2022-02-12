52969eebe6e6a73536e21638aa5eb4e3699ba1d6
hexxa <hexxa@outlook.com>
2022-02-12 11:03:00 +0800

    feat(fe/cron): add cron table

---
+++ src/client/web/src/common/cron.ts
@@ -0,0 +1,40 @@
+import { Map } from "immutable";
+
+export interface CronTask {
+  func: (arg?: any, ...optionalArgs: any[]) => void;
+  delay: number;
+  args?: any[];
+  handler?: number;
+}
+
+export class Cron {
+  private tasks: Map<string, CronTask>;
+  constructor() {
+    this.tasks = Map<string, CronTask>();
+  }
+
+  add = (name: string, task: CronTask) => {
+    if (this.tasks.has(name)) {
+      this.delete(name);
+    }
+
+    const handler = window.setInterval(task.func, task.delay, ...task.args);
+    task.handler = handler;
+    this.tasks = this.tasks.set(name, task);
+  };
+
+  delete = (name: string) => {
+    const preTask = this.tasks.get(name);
+    window.clearInterval(preTask.handler);
+    this.tasks = this.tasks.delete(name);
+  };
+
+  getTasks = (): Map<string, CronTask> => {
+    return this.tasks;
+  };
+}
+
+const cronTable = new Cron();
+export const CronTable = (): Cron => {
+  return cronTable;
+};
