254a106f5d4204c6b7caf776edcdec9747621f1b
hexxa <hexxa@outlook.com>
2022-03-19 19:14:04 +0800

    fix(fe): fix incorrect chinses font

--- src/client/web/src/components/visual/title.tsx
+++ src/client/web/src/components/visual/title.tsx
@@ -2,7 +2,7 @@ import * as React from "react";
 import { List } from "immutable";
 
 import { Flexbox } from "../layout/flexbox";
-import { getIconWithProps } from "./icons";
+import { getIconWithProps, iconSize } from "./icons";
 import { colorClass } from "./colors";
 
 export interface Props {
@@ -15,15 +15,16 @@ export const Title = (props: Props) => {
   return (
     <Flexbox
       children={List([
-        <Flexbox
-          children={List([
-            getIconWithProps(props.iconName, {
-              size: "3.2rem",
+        <div>
+          <div className="float-l icon-l">
+            {getIconWithProps(props.iconName, {
+              size: iconSize("l"),
               className: `margin-r-m ${colorClass(props.iconColor)}-font`,
-            }),
-            <span className="title-m bold">{props.title}</span>,
-          ])}
-        />,
+            })}
+          </div>
+          <div className="title-m float-l bold">{props.title}</div>
+          <div className="fix"></div>
+        </div>,
 
         <span></span>,
       ])}
