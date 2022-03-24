fc69fc71d28b71805ecfc23878d981e90b143565
hexxa <hexxa@outlook.com>
2022-03-24 16:44:34 +0800

    fix(fileinfo_store): refactor file info store

--- src/db/fileinfostore/file_info_store.go
+++ src/db/fileinfostore/file_info_store.go
@@ -11,7 +11,6 @@ import (
 
 	"github.com/ihexxa/quickshare/src/db"
 	"github.com/ihexxa/quickshare/src/kvstore"
-	"github.com/ihexxa/quickshare/src/kvstore/boltdbpvd"
 )
 
 const (
@@ -50,16 +49,15 @@ type IFileInfoStore interface {
 }
 
 type FileInfoStore struct {
-	mtx    *sync.RWMutex
-	store  kvstore.IKVStore
-	boltdb boltdbpvd.BoltProvider
+	mtx   *sync.RWMutex
+	store kvstore.IKVStore
 }
 
 func NewFileInfoStore(store kvstore.IKVStore) (*FileInfoStore, error) {
 	var err error
 	for _, nsName := range []string{
-		InitNs,
-		db.InfoNs,
+		db.FileSchemaNs,
+		db.FileInfoNs,
 		db.ShareIDNs,
 	} {
 		if !store.HasNamespace(nsName) {
@@ -69,115 +67,15 @@ func NewFileInfoStore(store kvstore.IKVStore) (*FileInfoStore, error) {
 		}
 	}
 
-	boltdb := store.(boltdbpvd.BoltProvider)
-
 	fi := &FileInfoStore{
-		store:  store,
-		boltdb: boltdb,
-		mtx:    &sync.RWMutex{},
+		store: store,
+		mtx:   &sync.RWMutex{},
 	}
 	return fi, nil
 }
 
-func (fi *FileInfoStore) AddSharing(dirPath string) error {
-	fi.mtx.Lock()
-	defer fi.mtx.Unlock()
-
-	info, err := fi.GetInfo(dirPath)
-	if err != nil {
-		if !errors.Is(err, ErrNotFound) {
-			return err
-		}
-		info = &db.FileInfo{
-			IsDir: true,
-		}
-	}
-
-	// TODO: ensure Atomicity
-	shareID, err := fi.getShareID(dirPath)
-	if err != nil {
-		return err
-	}
-	err = fi.store.SetStringIn(db.ShareIDNs, shareID, dirPath)
-	if err != nil {
-		return err
-	}
-
-	info.Shared = true
-	info.ShareID = shareID
-	return fi.SetInfo(dirPath, info)
-}
-
-func (fi *FileInfoStore) DelSharing(dirPath string) error {
-	fi.mtx.Lock()
-	defer fi.mtx.Unlock()
-
-	info, err := fi.GetInfo(dirPath)
-	if err != nil {
-		return err
-	}
-
-	// TODO: ensure Atomicity
-	// In the bolt, if the key does not exist
-	// then nothing is done and a nil error is returned
-
-	// because before this version, shareIDs are not removed correctly
-	// so it iterates all shareIDs and cleans remaining entries
-	shareIDtoDir, err := fi.store.ListStringsIn(db.ShareIDNs)
-	if err != nil {
-		return err
-	}
-
-	for shareID, shareDir := range shareIDtoDir {
-		if shareDir == dirPath {
-			err = fi.store.DelStringIn(db.ShareIDNs, shareID)
-			if err != nil {
-				return err
-			}
-		}
-	}
-
-	info.ShareID = ""
-	info.Shared = false
-	return fi.SetInfo(dirPath, info)
-}
-
-func (fi *FileInfoStore) GetSharing(dirPath string) (bool, bool) {
-	fi.mtx.Lock()
-	defer fi.mtx.Unlock()
-
-	// TODO: differentiate error and not exist
-	info, err := fi.GetInfo(dirPath)
-	if err != nil {
-		return false, false
-	}
-	return info.IsDir && info.Shared, true
-}
-
-func (fi *FileInfoStore) ListSharings(prefix string) (map[string]string, error) {
-	infoStrs, err := fi.store.ListStringsByPrefixIn(prefix, db.InfoNs)
-	if err != nil {
-		return nil, err
-	}
-
-	info := &db.FileInfo{}
-	sharings := map[string]string{}
-	for itemPath, infoStr := range infoStrs {
-		err = json.Unmarshal([]byte(infoStr), info)
-		if err != nil {
-			return nil, fmt.Errorf("list sharing error: %w", err)
-		}
-
-		if info.IsDir && info.Shared {
-			sharings[itemPath] = info.ShareID
-		}
-	}
-
-	return sharings, nil
-}
-
-func (fi *FileInfoStore) GetInfo(itemPath string) (*db.FileInfo, error) {
-	infoStr, ok := fi.store.GetStringIn(db.InfoNs, itemPath)
+func (fi *FileInfoStore) getInfo(itemPath string) (*db.FileInfo, error) {
+	infoStr, ok := fi.store.GetStringIn(db.FileInfoNs, itemPath)
 	if !ok {
 		return nil, ErrNotFound
 	}
@@ -190,10 +88,14 @@ func (fi *FileInfoStore) GetInfo(itemPath string) (*db.FileInfo, error) {
 	return info, nil
 }
 
+func (fi *FileInfoStore) GetInfo(itemPath string) (*db.FileInfo, error) {
+	return fi.getInfo(itemPath)
+}
+
 func (fi *FileInfoStore) GetInfos(itemPaths []string) (map[string]*db.FileInfo, error) {
 	infos := map[string]*db.FileInfo{}
 	for _, itemPath := range itemPaths {
-		info, err := fi.GetInfo(itemPath)
+		info, err := fi.getInfo(itemPath)
 		if err != nil {
 			if !errors.Is(err, ErrNotFound) {
 				return nil, err
@@ -206,28 +108,34 @@ func (fi *FileInfoStore) GetInfos(itemPaths []string) (map[string]*db.FileInfo, 
 	return infos, nil
 }
 
-func (fi *FileInfoStore) SetInfo(itemPath string, info *db.FileInfo) error {
+func (fi *FileInfoStore) setInfo(itemPath string, info *db.FileInfo) error {
 	infoStr, err := json.Marshal(info)
 	if err != nil {
 		return fmt.Errorf("set file info: %w", err)
 	}
 
-	err = fi.store.SetStringIn(db.InfoNs, itemPath, string(infoStr))
+	err = fi.store.SetStringIn(db.FileInfoNs, itemPath, string(infoStr))
 	if err != nil {
 		return fmt.Errorf("set file info: %w", err)
 	}
 	return nil
 }
 
-func (fi *FileInfoStore) DelInfo(itemPath string) error {
-	return fi.store.DelStringIn(db.InfoNs, itemPath)
+func (fi *FileInfoStore) SetInfo(itemPath string, info *db.FileInfo) error {
+	return fi.setInfo(itemPath, info)
 }
+
+func (fi *FileInfoStore) DelInfo(itemPath string) error {
+	return fi.store.DelStringIn(db.FileInfoNs, itemPath)
+}
+
+// sharings
 
 func (fi *FileInfoStore) SetSha1(itemPath, sign string) error {
 	fi.mtx.Lock()
 	defer fi.mtx.Unlock()
 
-	info, err := fi.GetInfo(itemPath)
+	info, err := fi.getInfo(itemPath)
 	if err != nil {
 		if !errors.Is(err, ErrNotFound) {
 			return err
@@ -238,7 +146,7 @@ func (fi *FileInfoStore) SetSha1(itemPath, sign string) error {
 		}
 	}
 	info.Sha1 = sign
-	return fi.SetInfo(itemPath, info)
+	return fi.setInfo(itemPath, info)
 }
 
 func (fi *FileInfoStore) getShareID(payload string) (string, error) {
@@ -272,4 +180,101 @@ func (fi *FileInfoStore) GetSharingDir(hashID string) (string, error) {
 		return "", ErrSharingNotFound
 	}
 	return dirPath, nil
+}
+
+func (fi *FileInfoStore) AddSharing(dirPath string) error {
+	fi.mtx.Lock()
+	defer fi.mtx.Unlock()
+
+	info, err := fi.getInfo(dirPath)
+	if err != nil {
+		if !errors.Is(err, ErrNotFound) {
+			return err
+		}
+		info = &db.FileInfo{
+			IsDir: true,
+		}
+	}
+
+	// TODO: ensure Atomicity
+	shareID, err := fi.getShareID(dirPath)
+	if err != nil {
+		return err
+	}
+	err = fi.store.SetStringIn(db.ShareIDNs, shareID, dirPath)
+	if err != nil {
+		return err
+	}
+
+	info.Shared = true
+	info.ShareID = shareID
+	return fi.setInfo(dirPath, info)
+}
+
+func (fi *FileInfoStore) DelSharing(dirPath string) error {
+	fi.mtx.Lock()
+	defer fi.mtx.Unlock()
+
+	info, err := fi.getInfo(dirPath)
+	if err != nil {
+		return err
+	}
+
+	// TODO: ensure Atomicity
+	// In the bolt, if the key does not exist
+	// then nothing is done and a nil error is returned
+
+	// because before this version, shareIDs are not removed correctly
+	// so it iterates all shareIDs and cleans remaining entries
+	shareIDtoDir, err := fi.store.ListStringsIn(db.ShareIDNs)
+	if err != nil {
+		return err
+	}
+
+	for shareID, shareDir := range shareIDtoDir {
+		if shareDir == dirPath {
+			err = fi.store.DelStringIn(db.ShareIDNs, shareID)
+			if err != nil {
+				return err
+			}
+		}
+	}
+
+	info.ShareID = ""
+	info.Shared = false
+	return fi.setInfo(dirPath, info)
+}
+
+func (fi *FileInfoStore) GetSharing(dirPath string) (bool, bool) {
+	fi.mtx.Lock()
+	defer fi.mtx.Unlock()
+
+	// TODO: differentiate error and not exist
+	info, err := fi.getInfo(dirPath)
+	if err != nil {
+		return false, false
+	}
+	return info.IsDir && info.Shared, true
+}
+
+func (fi *FileInfoStore) ListSharings(prefix string) (map[string]string, error) {
+	infoStrs, err := fi.store.ListStringsByPrefixIn(prefix, db.FileInfoNs)
+	if err != nil {
+		return nil, err
+	}
+
+	info := &db.FileInfo{}
+	sharings := map[string]string{}
+	for itemPath, infoStr := range infoStrs {
+		err = json.Unmarshal([]byte(infoStr), info)
+		if err != nil {
+			return nil, fmt.Errorf("list sharing error: %w", err)
+		}
+
+		if info.IsDir && info.Shared {
+			sharings[itemPath] = info.ShareID
+		}
+	}
+
+	return sharings, nil
 }
