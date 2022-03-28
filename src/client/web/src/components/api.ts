e4940fca3420d9eba5a64982ad9ddfdd7b85fbb5
hexxa <hexxa@outlook.com>
2022-03-28 15:58:57 +0800

    feat(fe/api): expose quickshare api in the frontend dev mode

---
+++ src/client/web/src/components/api.ts
@@ -0,0 +1,42 @@
+import { ICoreState } from "./core_state";
+import { updater, Updater } from "./state_updater";
+import { UploadEntry } from "../worker/interface";
+
+export class QuickshareAPI {
+  private updater: Updater;
+  constructor() {
+    this.updater = updater();
+  }
+  initAll = async (params: URLSearchParams): Promise<string> => {
+    return this.updater.initAll(params);
+  }
+
+  addUploadArray = (fileArray: Array<File>): string => {
+    return this.updater.addUploadArray(fileArray);
+  };
+
+  deleteUpload = async (filePath: string): Promise<string> => {
+    return await this.updater.deleteUpload(filePath);
+  };
+
+  listUploadArray = async (): Promise<Array<UploadEntry>> => {
+    return await this.updater.listUploadArray();
+  };
+
+  stopUploading = (filePath: string): string => {
+    return this.updater.stopUploading(filePath);
+  }
+
+  self = async (): Promise<string> => {
+    return await this.updater.self();
+  };
+
+  getProps = (): ICoreState => {
+    return this.updater.props;
+  };
+}
+
+const api = new QuickshareAPI();
+export const API = (): QuickshareAPI => {
+  return api;
+};
