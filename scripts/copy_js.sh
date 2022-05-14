36cf1f52c98ffacbc0c0cbd4f14e7d4cdfdcbbbf
hexxa <hexxa@outlook.com>
2022-05-14 14:59:44 +0800

    fix(ci): js asset folder is missing

--- scripts/copy_js.sh
+++ scripts/copy_js.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 set -o nounset errexit
 
-export ROOT=`pwd`
-cp $ROOT/node_modules/immutable/dist/immutable.min.js $ROOT/static/public/js/
-cp $ROOT/node_modules/react-dom/umd/react-dom.production.min.js $ROOT/static/public/js/
-cp $ROOT/node_modules/react/umd/react.production.min.js $ROOT/static/public/js
+ROOT=$(pwd)
+export ROOT
+
+mkdir -p "$ROOT/static/public/js"
+cp "$ROOT/node_modules/immutable/dist/immutable.min.js" "$ROOT/static/public/js/"
+cp "$ROOT/node_modules/react-dom/umd/react-dom.production.min.js" "$ROOT/static/public/js/"
+cp "$ROOT/node_modules/react/umd/react.production.min.js" "$ROOT/static/public/js/"
