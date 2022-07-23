bd2e831a5de95ce9072d0eed03c437fe4556ab67
hexxa <hexxa@outlook.com>
2022-07-23 22:24:32 +0800

    test(server/search): add tests for searching

--- src/server/server_concurrency_test.go
+++ src/server/server_concurrency_test.go
@@ -10,7 +10,7 @@ import (
 	q "github.com/ihexxa/quickshare/src/handlers"
 )
 
-func xTestConcurrency(t *testing.T) {
+func TestConcurrency(t *testing.T) {
 	addr := "http://127.0.0.1:8686"
 	rootPath := "tmpTestData"
 	config := `{
